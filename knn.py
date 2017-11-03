b16c73085c9b1c30ec2be1d204ebf5cb0ee1c716
tbeiko <t.beiko23@gmail.com>
2017-11-03 10:23:02 -0400

    Load train and test data to memory

--- knn.py
+++ knn.py
@@ -1,4 +1,57 @@
+import numpy as np 
+import os 
+
 # ============ PREPROCESSING ============
+# Need to create set of files with normalized length
+NORMALIZED_LENGTH = 20 # Need to change this, or just get it from files which all have the same length
+XTrain = []
+YTrain = []
+XTest = []
+YTest = []
+
+print "Loading input to memory"
+for filename in os.listdir(os.getcwd()+ "/sequences"):
+    current_file = open("sequences/" + filename, 'r')
+    length_count = 0 
+
+    # Training Input
+    if "trainimg" in filename and "inputdata" in filename:
+        X = []  
+        while length_count < NORMALIZED_LENGTH:
+            line = current_file.readline().split()
+            length_count += 1
+            X.append(map(int, line))
+        XTrain.append(X)
+
+    # Training Output
+    elif "trainimg" in filename and "targetdata" in filename:
+        line = current_file.readline().split()
+        digit_class = 0 
+        for i in range(0, 10):
+            if int(line[i]) == 1:
+                digit_class = i
+                break
+        YTrain.append(digit_class)
+
+
+    # Testing Input
+    elif "testimg" in filename and "inputdata" in filename:
+        X = []  
+        while length_count < NORMALIZED_LENGTH:
+            line = current_file.readline().split()
+            length_count += 1
+            X.append(map(int, line))
+        XTest.append(X)
+
+    # Testing Output 
+    elif "testimg" in filename and "targetdata" in filename:
+        line = current_file.readline().split()
+        digit_class = 0 
+        for i in range(0, 10):
+            if int(line[i]) == 1:
+                digit_class = i
+                break
+        YTest.append(digit_class)
 
 # =======================================
 
