6d5ae7e16a3b45c2947f76f2814cc26b0e9647a4
Hessam Amini <33270761+hessamamini@users.noreply.github.com>
2017-11-08 23:58:31 -0500

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,41 @@
+import numpy as np
+import os
+from scipy import signal
+
+NORMALIZED_LENGTH = int(np.round(39.86897142857143))
+#NORMALIZED_LENGTH = 117
+DATA_FOLDER = "/sequences"
+PREPROCESSED_FOLDER = "/preprocessed_sequences"
+
+for filename in os.listdir(os.getcwd()+ DATA_FOLDER):
+    current_file = open(DATA_FOLDER[1 :] + '/' + filename, 'r')
+
+    if "inputdata" in filename:
+        X = []
+        lines = current_file.readlines()
+        for line in lines:
+            X.append([])
+            currentLine = line.split()
+            for num in currentLine:
+                X[-1].append(float(num))
+        X = np.array(X)
+
+        X = np.transpose(X)
+        newX = []
+        for row in X:
+            newX.append(signal.resample(row, NORMALIZED_LENGTH))
+        newX = np.transpose(np.array(newX))
+
+        with open(PREPROCESSED_FOLDER[1 :] + '/' + filename, 'w') as file:
+            for row in newX:
+                file.write(str(row[0]))
+                for element in row[1 :]:
+                    file.write(' ' + str(element))
+                file.write('\n')
+
+    if "targetdata" in filename:
+        line = current_file.readline().split()
+        with open(PREPROCESSED_FOLDER[1:] + '/' + filename, 'w') as file:
+            file.write(str(line[0]))
+            for i in range(1, 10):
+                file.write(' ' + str(line[i]))
